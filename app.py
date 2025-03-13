997fc8852ce4001b290a9bd4ddbe5d8c969fa70d
sakeena majeed <sakeenamajeed24@gmail.com>
2025-03-13 19:11:43 +0500

    host

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import streamlit as st
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
+        host="https://personal-library-manager-sakeena.streamlit.app/",
         user="Sakeena",  
         password="03701131858", 
         database="library_db"
